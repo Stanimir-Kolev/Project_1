ba7140f3a6c8fa892b55c2928d4a8c7783b52808
Ivan Valkov <i.valkov92@gmail.com>
2017-09-22 18:10:52 +0300

    Created carousel/aside elemets, sections and articles

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,16 +1,16 @@
-var search = document.getElementById('search');
-var show = document.getElementById('show');
-search.addEventListener('click', function () {
-    show.style.display = 'inline';
-}, false);
-var close = document.getElementById('close');
-close.style.cursor = 'pointer';
-close.addEventListener('click', function () {
-    show.style.display = 'none';
-}, false)
+// var search = document.getElementById('search');
+// var show = document.getElementById('show');
+// search.addEventListener('click', function () {
+//     show.style.display = 'inline';
+// }, false);
+// var close = document.getElementById('close');
+// close.style.cursor = 'pointer';
+// close.addEventListener('click', function () {
+//     show.style.display = 'none';
+// }, false)
 
-var snimki = document.getElementsByClassName('pic');
-var mas = Array.prototype.slice.call(snimki);
+// var snimki = document.getElementsByClassName('pic');
+// var mas = Array.prototype.slice.call(snimki);
 
 // mas.forEach(function (x) {
 //     x.addEventListener('mouseover', function () {
